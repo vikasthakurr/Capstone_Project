4f5636bcb9165c87a5b4e0ed46ff79632bf041ff
vikasthakurr <vikaskumar20012001@gmail.com>
2023-02-06 01:02:59 +0530

    home page added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,26 +4,28 @@ import "../Stylesheets/Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <div className="navbar">
-        <div className="nav-links">
-          <h2>The Dine</h2>
-          <ul>
-            <li><a href="" className="active">Home</a></li>
-            <li><a href="">Menu</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Order</a></li>
-          </ul>
+      <div className="home">
+        <div className="navbar">
+          <div className="nav-links">
+            <h2>The Dine</h2>
+            <ul>
+              <li><a href="" className="active">Home</a></li>
+              <li><a href="">Menu</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Order</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="hero">
+          <div className="hero-content">
+            <h1>Welcome to <span>The Dine</span></h1>
+          </div>
+          <div className="button-more">
+            <a href="">Know More</a>
+          </div>
+
         </div>
       </div>
-      <div className="hero">
-            <div className="hero-content">
-               <h1>Welcome to <span>The Dine</span></h1>
-            </div>
-            <div className="button-more">
-               <a href="">Know More</a>
-            </div>
-
-         </div>
     </>
   )
 }
