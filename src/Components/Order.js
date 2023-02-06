e0ee5ea4e2e6b7ce005a3a8ccec5fb07fac0e468
vikasthakurr <vikaskumar20012001@gmail.com>
2023-02-07 01:07:58 +0530

    booking/order page added

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -5,10 +5,22 @@ const Order = () => {
   return (
     <>
       <div className="order">
-      <div className="order-heading">
-        <h1>Book A Table</h1>
-      </div>
-        
+        <div className="order-heading">
+          <h1>Book A Table</h1>
+        </div>
+
+        <div className="order-form">
+          <div className="form-box">
+            <input type="text" placeholder="Your name..."/>
+            <input type="text" placeholder="Mobile..." />
+            <input type="email" placeholder="Email..." />
+            <input type="text" placeholder="No. of person..." />
+            <input type="date" className="dated" />
+          </div>
+        </div>
+
+        <a href="">Book Now</a>
+
       </div>
     </>
   )
