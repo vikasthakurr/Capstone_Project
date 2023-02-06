08319c8e8101cfbd0b8926508ddfd9d89443397c
vikasthakurr <vikaskumar20012001@gmail.com>
2023-02-06 10:57:47 +0530

    Menu and discount page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Service from './Components/Service';
 
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Service/>
     </>
   );
 }
--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import "../Stylesheets/Services.css";
+// image imported here
+import card_1 from "../Photos/card-1.jpg";
+import card_2 from "../Photos/card-2.jpg";
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <>
+      <div className="container-box">
+        <div className="box">
+          <h1>Special Discount Days</h1>
+        </div>
+        <div className="offers">
+          <div className="cards">
+            <div className="imgs">
+              <img src={card_1} />
+            </div>
+            <div className="contents">
+              <h2>Midnight Cravings</h2>
+              <p>30% Off</p>
+              <button className="order-btn">Place Order <i class="fa-solid fa-cart-shopping"></i></button>
+            </div>
+          </div>
+
+          {/* card-2 */}
+          <div className="cards">
+            <div className="imgs">
+              <img src={card_2} />
+            </div>
+            <div className="contents">
+              <h2>Family Weekend</h2>
+              <p>50% Off</p>
+              <button className="order-btn">Place Order <i class="fa-solid fa-cart-shopping"></i></button>
+            </div>
+          </div>
+        </div>
+
+        <div className="menu-heading">
+          <div className="heading">
+            <h1>Ours Specials</h1>
+            <p>Main Course</p>
+            <hr />
+          </div>
+        </div>
+      </div>
+
+     
+    </>
   )
 }
 
